36f272014e87347d8ece7e4d78131fc492c84e3d
Keli Hansen <keli.p.hansen@gmail.com>
2018-04-14 12:39:33 -0700

    yay failing test

--- test/app.test.js
+++ test/app.test.js
@@ -2,6 +2,7 @@ require('dotenv').config({ path: './test/.env' });
 const mongo = require('../lib/mongodb');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const app = require('../lib/app');
 
 chai.use(chaiHttp);
 const { assert } = chai;
@@ -13,8 +14,23 @@ describe('birds API', () => {
         });
     });
 
-    it('has no birds', () => {
-        assert(true);
+    let hummingbird = {
+        commonName: 'Anna\'s hummingbird',
+        scientificName: 'Calypte anna',
+        wingspan: '12 cm',
+        diet: 'nectar',
+        colors: ['green', 'gray', 'red']
+    };
+
+    it('saves a bird', () => {
+        return chai.request(app)
+            .post('/birds')
+            .send(hummingbird)
+            .then(({ body }) => {
+                assert.ok(body._id);
+                assert.equal(body.scientificName, hummingbird.scientificName);
+                hummingbird = body;
+            });
     });
 
     after(() => mongo.client.close());
